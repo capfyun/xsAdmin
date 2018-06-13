b5d8a6503b1188b258cc2cad6ed30684f20da4a5
xiashuang <550373770@qq.com>
2018-06-13 11:35:45 +0800

    kafka测试

--- application/admin/controller/Debug.php
+++ application/admin/controller/Debug.php
@@ -22,6 +22,7 @@ class Debug extends \app\common\controller\AdminBase{
 	 */
 	public function test(){
 		
+		
 		$a = Rsa::publics('../openssl/rsa_public_key.pem')->encrypt('asdasd');
 		
 		halt($a);
@@ -29,4 +30,50 @@ class Debug extends \app\common\controller\AdminBase{
 		return $this->fetch();
 	}
 	
+	
+	public function produce(){
+
+		// 设置生产相关配置，具体配置参数见 [Configuration](Configuration.md)
+		$config = \Kafka\ProducerConfig::getInstance();
+		$config->setMetadataRefreshIntervalMs(10000);
+		$config->setMetadataBrokerList('10.13.4.159:9192');
+		$config->setBrokerVersion('0.9.0.1');
+		$config->setRequiredAck(1);
+		$config->setIsAsyn(false);
+		$config->setProduceInterval(500);
+		$producer = new \Kafka\Producer(function() {
+			return array(
+				array(
+					'topic' => 'test',
+					'value' => 'test....message.',
+					'key' => 'testkey',
+				),
+			);
+		});
+//		$producer->setLogger($logger);
+		$producer->success(function($result) {
+			var_dump($result);
+		});
+		$producer->error(function($errorCode, $context) {
+			var_dump($errorCode);
+		});
+		$producer->send();
+	}
+	
+	
+	public function consumer(){
+		
+		$config = \Kafka\ConsumerConfig::getInstance();
+		$config->setMetadataRefreshIntervalMs(10000);
+		$config->setMetadataBrokerList('10.13.4.159:9192');
+		$config->setGroupId('test');
+		$config->setBrokerVersion('0.9.0.1');
+		$config->setTopics(array('test'));
+//$config->setOffsetReset('earliest');
+		$consumer = new \Kafka\Consumer();
+//		$consumer->setLogger($logger);
+		$consumer->start(function($topic, $part, $message) {
+			var_dump($message);
+		});
+	}
 }
